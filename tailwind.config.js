2ae5dd4580998226d0e3f72bb63a833f72d3dcc8
Harish Hareendra Babu <harish.babu@codurance.com>
2025-02-18 23:02:35 +0000

    Add native comments system with Netlify forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["404.html", "_layouts/default.html", "_layouts/home.html", "_includes/header.html", "_includes/footer.html"],
+  content: [
+    "404.html",
+    "_layouts/default.html",
+    "_layouts/home.html",
+    "_includes/header.html",
+    "_includes/footer.html",
+    "_includes/comments.html"],
   darkMode: 'media',
   theme: {
     extend: {},
