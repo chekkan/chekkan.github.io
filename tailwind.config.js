06c25887436e874e98b09a4e5765543c3aaacdd5
Harish Hareendra Babu <harish.babu@hey.com>
2025-04-08 10:09:55 +0100

    feat: decided on a new font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
     "404.html",
     "_layouts/default.html",
     "_layouts/home.html",
+    "_layouts/post.html",
     "_includes/header.html",
     "_includes/footer.html",
     "_includes/comments.html"],
   darkMode: 'media',
   theme: {
+    fontFamily: {
+      'serif': ['Petrona', '"Helvetica Neueu"', 'helvetica', '"Apple Color Emoji"', 'arial', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
